001c11c235e40fef036eb3e72c4d103353941849
hmaskai <hmaskai@asu.edu>
2016-04-17 01:34:45 -0700

    adding database

---
+++ Quizopedia/database/quizopedia.sql
@@ -0,0 +1,151 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 17, 2016 at 10:33 AM
+-- Server version: 10.1.10-MariaDB
+-- PHP Version: 5.6.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `quizopedia`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `demographics`
+--
+
+CREATE TABLE `demographics` (
+  `asu_id` int(10) NOT NULL,
+  `confidence` varchar(50) NOT NULL,
+  `courses_completed` int(1) NOT NULL,
+  `description` varchar(50) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `age` varchar(10) NOT NULL,
+  `mother_tongue` varchar(10) NOT NULL,
+  `country_of_residence` varchar(20) NOT NULL,
+  `other_country` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='collects demographic information during registration';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `username` varchar(30) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `fname` varchar(20) DEFAULT NULL,
+  `lname` varchar(20) DEFAULT NULL,
+  `gender` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Used for registration and login';
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`username`, `password`, `user_id`, `fname`, `lname`, `gender`) VALUES
+('hmaskai@asu.edu', 'harshil', 16, 'Harshil', 'Maskai', 'male'),
+('anuran@gmail.com', 'anuran', 21, 'Anuran', 'Duttaroy', 'male'),
+('skardile@asu.edu', 'saurabh', 24, 'Saurabh', 'Kardile', 'male');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE `questions` (
+  `question_id` int(10) NOT NULL,
+  `question_text` varchar(1000) NOT NULL,
+  `option_1` varchar(500) NOT NULL,
+  `option_2` varchar(500) NOT NULL,
+  `option_3` varchar(500) NOT NULL,
+  `option_4` varchar(500) NOT NULL,
+  `correct_answer` int(1) NOT NULL,
+  `tags` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `questions`
+--
+
+INSERT INTO `questions` (`question_id`, `question_text`, `option_1`, `option_2`, `option_3`, `option_4`, `correct_answer`, `tags`) VALUES
+(1, 'Which inheritance does java not support?', 'multiple', 'multilevel', 'single', 'runtime', 1, 'inheritance'),
+(2, 'How is unused memory cleared in java?', 'garbage bins', 'garbage collectors', 'crawlers', 'destructors', 2, 'garbage collection');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_questions`
+--
+
+CREATE TABLE `student_questions` (
+  `user_id` int(11) NOT NULL,
+  `question_id` int(10) NOT NULL,
+  `answer` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `student_questions`
+--
+
+INSERT INTO `student_questions` (`user_id`, `question_id`, `answer`) VALUES
+(24, 2, 1),
+(16, 2, 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `questions`
+--
+ALTER TABLE `questions`
+  ADD PRIMARY KEY (`question_id`),
+  ADD KEY `question_id` (`question_id`);
+
+--
+-- Indexes for table `student_questions`
+--
+ALTER TABLE `student_questions`
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `question_id` (`question_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+--
+-- AUTO_INCREMENT for table `questions`
+--
+ALTER TABLE `questions`
+  MODIFY `question_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
