fa63aa42f41f452523c7274264517885fd2ed9aa
Anuran Duttaroy <anuran420@gmail.com>
2016-04-16 20:13:00 -0700

    updated logout.php

--- Quizopedia/public/logout.php
+++ Quizopedia/public/logout.php
@@ -5,9 +5,5 @@ include_once("../includes/config.php");
 include_once("../includes/database.php");
 
 $session->logout();
-session_unset(); //unset variables
 
-session_destroy(); //destroy the session
-header("location: login.php"); //to redirect back to "index.php" after logging out
-exit();
 ?>
